7ce3e1616697cd9a740bc93726b6bc139d89a73d
jacoblovins <lovins.jacob@yahoo.com>
2020-07-16 22:06:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 $("document").ready(function() {
 
 
-    //click handler for the search button gathers all user data
-    $("#search").on("click", function(event) {
-        event.preventDefault();
-        var searchInput = $("#searchInput").val();
-        var recordsNum = $("#recordsNum").val();
-        var startInput = $("#startInput").val();
-        var endInput = $("#endInput").val();
-        console.log(startInput);
-        console.log(endInput);
-        console.log(recordsNum);
-        console.log(searchInput);
+  //click handler for the search button gathers all user data
+  $("#search").on("click", function(event) {
+      event.preventDefault();
+      var searchInput = $("#searchInput").val();
+      var recordsNum = $("#recordsNum").val();
+      var startInput = $("#startInput").val();
+      var endInput = $("#endInput").val();
+      var startYear = startInput + "0101";
+      var endYear = endInput + "1231";
+      console.log(startInput);
+      console.log(endInput);
+      console.log(recordsNum);
+      console.log(searchInput);
 
+      var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchInput + "&api-key=yphqAtha1ohzADvpzcslkLfFIZ2BWbJ5&begin_date=" + startYear + "&end_date=" + endYear + ""
+      
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        // $(".articles-body").empty();
+          console.log(response)
+          console.log(response.response.docs)
 
-        displayArticles();
-    })
+          var recordsSelect = $("#recordsNum").val();
+      
+          for(i=0;i<recordsSelect;i++) {
+            $("#cardBody").append("<div>"+response.response.docs[i].headline.main+"</div>");
+          }
+            
+        });
+  
 
-    //click handler for the clear button removes all user data
-    $("#clear").on("click", function(){
-        $("input").empty();
-    })
+      
+  })
 
-
-//function clears old search results and displays new results
-function displayArticles() {
-    $(".articles-body").empty();
-
-    var recordsSelect = $("#recordsNum").val();
-
-    for(i=0;i<recordsSelect;i++) {
-
-        var article = $("<div>");
-
-        article.addClass("article");
-
-        $(".articles-body").append(article);
-    }
-
-}
-
-})
-
+  //click handler for the clear button removes all user data
+  $("#clear").on("click", function(){
+      $("input").empty();
+  })
+});
